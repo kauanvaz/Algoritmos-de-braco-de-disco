772fc3c9398888e0487ad4369c36b6fc1565d678
Kauan <kauanvazc@gmail.com>
2022-10-01 12:28:27 -0300

    Comentários

--- main.py
+++ main.py
@@ -11,9 +11,12 @@ entrada = organiza_entrada()
 ult_cilindro = entrada[0]
 requisicoes = entrada[1:]
 
+# --------------------------------------- FCFS ------------------------------------------------
+
 quant_FCFS = 0
 for i in range(len(requisicoes)-1):
-    calculo = abs(requisicoes[i] - requisicoes[i+1]) # Cálculo da distância de requisições sequenciais
+	# Cálculo da distância de requisições sequenciais
+    calculo = abs(requisicoes[i] - requisicoes[i+1])
     quant_FCFS += calculo
 
 print("FCFS", quant_FCFS)
@@ -21,12 +24,15 @@ del quant_FCFS
 
 quant_SSTF = 0
 
+# --------------------------------------- SSTF ------------------------------------------------
+
 requi_SSTF = requisicoes.copy()
 head = requi_SSTF[0] # Posição da cabeça de leitura
 
 for i in range(len(requisicoes)-1):
-	candidatos = [abs(head-x) for x in requi_SSTF if x != head] # Calcula lista das distâncias do cilindro onde a
-																# cabeça de leitura está para os outros cilindros
+	# Calcula lista das distâncias do cilindro onde está a cabeça de leitura para os outros cilindros
+	candidatos = [abs(head-x) for x in requi_SSTF if x != head]
+																
 	candidato = min(candidatos) # O candidato escolhido é o que estiver mais próximo
 	ind_cand = candidatos.index(candidato)
 
@@ -38,12 +44,15 @@ for i in range(len(requisicoes)-1):
 print("SSTF", quant_SSTF)
 del quant_SSTF, requi_SSTF, head
 
+# -------------------------------------- ELEVADOR ------------------------------------------------
+
 requi_ELEVADOR = requisicoes.copy()
 head = requi_ELEVADOR[0] # Posição da cabeça de leitura
 
 requi_ELEVADOR.sort()
 
-quant_ELEVADOR = abs(head-requi_ELEVADOR[-1]) + requi_ELEVADOR[-1]-requi_ELEVADOR[0] # Cálculo da movimentação total
-																					 # da cabeça de leitura
+# Cálculo da movimentação total da cabeça de leitura
+quant_ELEVADOR = abs(head-requi_ELEVADOR[-1]) + (requi_ELEVADOR[-1]-requi_ELEVADOR[0])
+																					 
 print("ELEVADOR", quant_ELEVADOR)
 del entrada, ult_cilindro, requisicoes, requi_ELEVADOR, head, quant_ELEVADOR
